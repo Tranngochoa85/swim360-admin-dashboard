c550eccb2eb9cba11aea45f116eb933491b130be
Tranngochoa85 <tranngochoa85@gmail.com>
2025-08-01 22:54:11 +0700

    refactor: Remove unnecessary React import from DashboardPage

--- src/pages/AdminDashboardPage.tsx
+++ src/pages/AdminDashboardPage.tsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { getPendingPools, approvePool, rejectPool } from '../api/adminApi';
 
